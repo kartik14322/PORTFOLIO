e8c00389d4c69af8847e9b67d3efdf24c6eb4535
kartik <kartiksahu562866@gmail.com>
2026-02-20 13:51:22 +0530

    Initial commit - Portfolio project

---
+++ src/constants.js
@@ -0,0 +1,258 @@
+// Skills Section Logo's
+import htmlLogo from './assets/tech_logo/html.png';
+import cssLogo from './assets/tech_logo/css.png';
+import javascriptLogo from './assets/tech_logo/javascript.png';
+import reactjsLogo from './assets/tech_logo/reactjs.png';
+import reduxLogo from './assets/tech_logo/redux.png';
+import nextjsLogo from './assets/tech_logo/nextjs.png';
+import tailwindcssLogo from './assets/tech_logo/tailwindcss.png';
+import materialuiLogo from './assets/tech_logo/materialui.png';
+import bootstrapLogo from './assets/tech_logo/bootstrap.png';
+import expressjsLogo from './assets/tech_logo/express.png';
+import mysqlLogo from './assets/tech_logo/mysql.png';
+import mongodbLogo from './assets/tech_logo/mongodb.png';
+import cLogo from './assets/tech_logo/c.png';
+import cppLogo from './assets/tech_logo/cpp.png';
+import javaLogo from './assets/tech_logo/java.png';
+import pythonLogo from './assets/tech_logo/python.png';
+import typescriptLogo from './assets/tech_logo/typescript.png';
+import gitLogo from './assets/tech_logo/git.png';
+import githubLogo from './assets/tech_logo/github.png';
+import vscodeLogo from './assets/tech_logo/vscode.png';
+import postmanLogo from './assets/tech_logo/postman.png';
+import vercelLogo from './assets/tech_logo/vercel.png';
+import SQLLOGO from './assets/tech_logo/SQL.png';
+import Render from './assets/tech_logo/RENDER.jpeg';
+import Docker from './assets/tech_logo/Docker.png';
+import SK from "./assets/tech_logo/SK.jpeg";
+import R from "./assets/tech_logo/REGRESSION.jpeg";
+import NODEJS from './assets/tech_logo/NODE.jpeg';
+import STREAMLIT from "./assets/tech_logo/STREAMLIT.png";
+import DJANGO from "./assets/tech_logo/DJANGO.png";
+import DSA from "./assets/tech_logo/DSA.jpg";
+import OOP from "./assets/tech_logo/OOP.png";
+import DBMS from "./assets/tech_logo/DBMS.jpg";
+import CN from "./assets/tech_logo/CN.png";
+import OS from "./assets/tech_logo/OS.png";
+import JUPYTER from "./assets/tech_logo/JUPETER.png";
+import CICD from "./assets/tech_logo/CI-CD.png";
+import SEABORN from "./assets/tech_logo/SEABORN.png";
+import MATPLOTLIB from "./assets/tech_logo/MATPLOTLIB.png";
+import POWERBI from "./assets/tech_logo/POWERBI.jpeg";
+import TABLUE from "./assets/tech_logo/TABLUE.png";
+import PLOTY from "./assets/tech_logo/PLOTLY.png";
+import KERAS from "./assets/tech_logo/keras.png";
+import ALGEBRA from "./assets/tech_logo/ALGEBRA.png";
+import CALCULUS from "./assets/tech_logo/CALCULUS.png";
+import PROBABILITY from "./assets/tech_logo/PROBABILITY.jpeg";
+import STATISTICS from "./assets/tech_logo/STATISTICS.png";
+import PYTROCH from "./assets/tech_logo/PYTROCH.jpeg";
+import TENSORFLOW from "./assets/tech_logo/TENSORFLOW.png";
+import TRANSFORMER from "./assets/tech_logo/TRANSFORMERS.jpeg";
+import RNN from "./assets/tech_logo/RNN.jpg";
+import CNN from "./assets/tech_logo/CNN.jpg";
+import MODELMONITERING from "./assets/tech_logo/MODELMONITERING.jpg";
+import NUMPY from "./assets/tech_logo/NUMPY.png";
+import PANDAS from "./assets/tech_logo/PANDAS.jpg";
+import SCIPY from "./assets/tech_logo/SCIPY.webp";
+import FLASK from "./assets/tech_logo/FLASK.jpeg";
+import CLUSTERING from "./assets/tech_logo/CALCULUS.png";
+import XG from "./assets/tech_logo/XG.jpg";
+import CLASSIFICATION from "./assets/tech_logo/CLASSIFICATION.jpg";
+import HT from "./assets/tech_logo/HP.jpeg";
+import CV from "./assets/tech_logo/CROSS.jpg";
+import DR from "./assets/tech_logo/DR.jpg";
+import FE from "./assets/tech_logo/FE.jpg";
+
+
+// Project Section Logo's
+import AIRBNB from './assets/work_logo/AIRBNB.png';
+import APNAVIDEOCOLL from './assets/work_logo/APNAVIDEOCOLL.png';
+import WEATHER from "./assets/work_logo/WHEATHER.png";
+import PriceComparison from "./assets/work_logo/price.png";
+import ImageScraping from "./assets/work_logo/scraping.png";
+
+
+
+export const SkillsInfo = [
+  {
+    title: 'Frontend',
+    skills: [
+      { name: 'HTML', logo: htmlLogo },
+      { name: 'CSS', logo: cssLogo },
+      { name: 'JavaScript', logo: javascriptLogo },
+      { name: 'React JS', logo: reactjsLogo },
+      { name: 'Redux', logo: reduxLogo },
+      { name: 'Next JS', logo: nextjsLogo },
+      { name: 'Tailwind CSS', logo: tailwindcssLogo },
+      { name: 'Material UI', logo: materialuiLogo },
+      { name: 'Bootstrap', logo: bootstrapLogo },
+    ],
+  },
+  {
+    title: 'Backend',
+    skills: [
+      { name: 'Node JS', logo: NODEJS },
+      { name: 'Streamlit', logo: STREAMLIT },
+      { name: 'Flask', logo: FLASK },
+      { name: 'Django', logo: DJANGO },
+      { name: 'Express JS', logo: expressjsLogo },
+      
+    ],
+  },
+   {
+    title: 'Database',
+    skills: [
+      { name: 'MySQL', logo: mysqlLogo },
+      { name: 'MongoDB', logo: mongodbLogo }
+    ],
+  },
+  {
+    title: 'Languages',
+    skills: [
+      { name: 'C', logo: cLogo },
+      { name: 'C++', logo: cppLogo },
+      { name: 'Java', logo: javaLogo },
+      { name: 'Python', logo: pythonLogo },
+      { name: 'JavaScript', logo: javascriptLogo },
+      { name: 'TypeScript', logo: typescriptLogo },
+      { name: 'SQL', logo: SQLLOGO }
+    ],
+  },
+  {
+    title: 'Tools',
+    skills: [
+      { name: 'Git', logo: gitLogo },
+      { name: 'GitHub', logo: githubLogo },
+      { name: 'VS Code', logo: vscodeLogo },
+      { name: 'Postman', logo: postmanLogo },
+      { name: 'Vercel', logo: vercelLogo },
+      { name: 'Render', logo: Render },
+      { name: 'Docker', logo: Docker },
+      { name: 'Jupyter Notebook', logo: JUPYTER },
+      { name: 'CI/CD for ML', logo: CICD },
+      // { name: 'Kubernetes', logo: cppLogo },
+
+    ],
+  },
+   {
+    title: 'Core Computer Science',
+    skills: [
+      { name: 'DSA', logo: DSA },
+      { name: 'OOPS', logo: OOP },
+      { name: 'DBMS', logo: DBMS },
+      { name: 'Operating Systems', logo: OS},
+      { name: 'Computer Networks', logo: CN }
+    ],
+  },
+  {
+    title: 'Data Analysis & Manipulation',
+    skills: [
+      { name: 'NumPy', logo: NUMPY },
+      { name: 'Pandas', logo: PANDAS  },
+      { name: 'SciPy', logo: SCIPY },
+      { name: 'Model Monitoring', logo: MODELMONITERING },
+    ],
+  },
+  {
+    title: 'Data Visualization',
+    skills: [
+      { name: 'Matplotlib', logo: MATPLOTLIB },
+      { name: 'Seaborn', logo: SEABORN },
+      { name: 'Plotly', logo: PLOTY },
+      { name: 'Power BI', logo:  POWERBI},
+      { name: 'Tableau', logo: TABLUE }
+    ],
+  },
+  
+  {
+    title: 'Deep Learning',
+    skills: [
+      { name: 'TensorFlow', logo: TENSORFLOW   },
+      { name: 'Keras', logo: KERAS },
+      { name: 'PyTorch', logo: PYTROCH },
+      { name: 'CNN', logo: CNN },
+      { name: 'Transformers', logo: TRANSFORMER },
+      { name: 'RNN', logo: RNN }
+    ],
+  },
+  {
+    title: 'Machine Learning',
+    skills: [
+      { name: 'Scikit-learn', logo: SK },
+      { name: 'Regression', logo: R },
+      { name: 'Classification', logo: CLASSIFICATION },
+      { name: 'Clustering', logo: CLUSTERING },
+      { name: 'Cross Validation', logo: CV },
+      { name: 'Hyperparameter Tuning', logo: HT },
+      { name: 'XGBoost', logo: XG },
+      { name: 'Dimensionality Reduction', logo: DR },
+      { name: 'Feature Engineering', logo: FE }
+    ],
+  },
+  {
+    title: 'Mathematics',
+    skills: [
+      { name: 'Linear Algebra', logo: ALGEBRA },
+      { name: 'Calculus', logo: CALCULUS },
+      { name: 'Statistics', logo: STATISTICS },
+      { name: 'Probability', logo: PROBABILITY }
+    ],
+  },
+];
+
+
+  export const projects = [
+    {
+      id: 0,
+      title: "AIRBNB Clone – Web Application",
+      description:
+        "The Airbnb Clone is a full-featured web application that replicates the core functionality of Airbnb, allowing users to explore property listings, view detailed information, and experience a modern booking interface. The project is built using React JS for the frontend, along with HTML, CSS, and JavaScript for structure, styling, and interactivity. Cloudinary is integrated for efficient image storage and optimization.",
+      image: AIRBNB,
+      tags: ["HTML", "CSS", "JavaScript", "Cloudinary","React JS", "MONGODB","NODE.JS"],
+      github: "https://github.com/kartik14322/AIRBNB",
+      webapp: "https://airbnb-kartik.onrender.com",
+    },
+    {
+      id: 1,
+      title: "Real-Time Video Calling Web Application",
+      description:
+        "APNA-VIDEO-CALL is a real-time video communication web application that enables users to connect with each other through secure peer-to-peer video and audio calls. The application is built using HTML, CSS, JavaScript, Node.js, and Socket.io, providing seamless real-time communication similar to platforms like Zoom or Google Meet.",
+      image: APNAVIDEOCOLL,
+      tags: ["React JS", "Node.js", "MongoDB", "SOCKET.IO", "HTML", "CSS", "JavaScript"],
+      github: "https://github.com/kartik14322/VIDEO_COLL_WEB",
+      webapp: "https://video-coll-kartik14320.onrender.com",
+    },
+    {
+      id: 2,
+      title: "Real-Time Medicine Price Comparison System",
+      description:
+        "This project is a Price Comparison and Intelligent Analysis System built using Python, SerpAPI, and Machine Learning (ML). The system automatically retrieves real-time product pricing data from multiple e-commerce platforms using SerpAPI, processes and stores the data, and applies ML techniques to analyze trends and predict optimal prices.",
+      image: PriceComparison,
+      tags: ["python", "Streamlit", "HTML", "CSS", "Render"],
+      github: "https://github.com/kartik14322/Medicine_Price_Compare",
+      webapp: "https://medicine-price-compare.onrender.com",
+    },
+    {
+      id: 3,
+      title: "Real-Time Image Scraping",
+      description:
+        "This project is an end-to-end image scraping and intelligent classification pipeline developed using Python and Machine Learning (ML). The system automatically collects high-resolution images from web sources, filters low-quality content, preprocesses the data, and trains a machine learning model for accurate image classification.",
+      image: ImageScraping,
+      tags: [ "Render", "Python", "HTML", "CSS", "Streamlit"],
+      github: "https://github.com/kartik14322/Image_scrapping_project",
+      webapp: "https://imagescrapping-kartik143.onrender.com",
+    },
+     {
+      id: 4,
+      title: "🌦️ Weather Web Application",
+      description:
+        "Developed a responsive Weather Web App that provides real-time weather updates including temperature, humidity, wind speed, and weather conditions for any city. The project uses API integration to fetch live data and displays it through a clean and user-friendly interface. Built with modern web technologies to ensure fast performance and smooth user experience.",
+      image: WEATHER,
+      tags: [ "Render", "React", "HTML", "CSS", "JS","NODE JS"],
+      github: "https://github.com/kartik14322/Weather_app",
+      webapp: "https://weatherapp-kartik143.onrender.com",
+    },
+  ];  
+
+
